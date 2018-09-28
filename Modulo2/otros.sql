3cd685b3bbd4ceb0d26dc16859ef7f018e1d688e
Aestevalis <43688124+Aestevalis@users.noreply.github.com>
2018-09-28 18:30:39 -0500

    Update otros.sql

--- Modulo2/otros.sql
+++ Modulo2/otros.sql
@@ -16,6 +16,8 @@ PURGE TABLESPACE diplo;
 # Estructura de tabla
 DESC tabla
 
+
+*******Clase Viernes 28 de Septiembre de 2018************
 connect hr
 psw: diplo
 
@@ -23,3 +25,14 @@ SELECT employee_id,first_name,last_name
 FROM employees
 order by 3,2;
 
+SELECT employee_id,first_name,last_name 
+FROM employees
+order by 3,2;
+
+spool 'M:\DiplomadoBD\Modulo2\salida.txt'
+/
+spool off
+
+SELECT distinct last_name, first_name
+from employees
+order by 1;
