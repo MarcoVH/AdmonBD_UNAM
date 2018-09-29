c92ab0414a8c9cdd3c484af03df8e00de2e933d8
MarcoVH <marcovazquezh@gmail.com>
2018-09-29 13:46:31 -0500

    Update insert_inmob.sql

--- Modulo2/bases_ejemplo/insert_inmob.sql
+++ Modulo2/bases_ejemplo/insert_inmob.sql
@@ -11,6 +11,11 @@ INSERT INTO staff VALUES('SA9','Mary','Howe','Assistant','F','19/02/1970','9000'
 INSERT INTO staff VALUES('SG5','Susan','Brand','Manager','F','03/06/1940','24000','B003');
 INSERT INTO staff VALUES('SL41','Julie','Lee','Assistant','F','13/06/1965','9000','B005');
 
+INSERT INTO privateowner VALUES('CO46','Joe','Keogh','2 Fergus Dr, Aberdeen AB2 7SX','01224-861212');
+INSERT INTO privateowner VALUES('CO87','Carol','Farrel','6 Achray St, Glasgow G32 9DX','0141-357-7419');
+INSERT INTO privateowner VALUES('CO40','Tina','Murphy','63 Well St, Glasgow G42','0141-943-1728');
+INSERT INTO privateowner VALUES('CO93','Tony','Shaw','12 Park Pl, Glasgow G4 0QR','0141-225-7025');
+
 INSERT INTO propertyforrent VALUES('PA14','16 Holhead','Aberdeen','AB7 5SU','House','6','650','CO46','SA9','B007');
 INSERT INTO propertyforrent VALUES('PL94','6Argyll St','London','NW2','Flat','4','400','CO87','SL41','B005');
 INSERT INTO propertyforrent VALUES('PG4','6 Lawrence St','Glasgow','G11 9QX','Flat','3','350','CO40','','B003');
@@ -23,13 +28,13 @@ INSERT INTO client VALUES('CR56','Aline','Stewart','0141-848-1825','Flat','350')
 INSERT INTO client VALUES('CR74','Mike','Ritchie','01475-392178','House','750');
 INSERT INTO client VALUES('CR62','Mary','Tregear','01224-196720','Flat','600');
 
-INSERT INTO privateowner VALUES('CO46','Joe','Keogh','2 Fergus Dr, Aberdeen AB2 7SX','01224-861212');
-INSERT INTO privateowner VALUES('CO87','Carol','Farrel','6 Achray St, Glasgow G32 9DX','0141-357-7419');
-INSERT INTO privateowner VALUES('CO40','Tina','Murphy','63 Well St, Glasgow G42','0141-943-1728');
-INSERT INTO privateowner VALUES('CO93','Tony','Shaw','12 Park Pl, Glasgow G4 0QR','0141-225-7025');
-
 INSERT INTO registration VALUES('CR76','B005','SL41','37988');
 INSERT INTO registration VALUES('CR56','B003','SG37','37722');
 INSERT INTO registration VALUES('CR74','B003','SG37','37576');
 INSERT INTO registration VALUES('CR62','B007','SA9','37687');
 
+INSERT INTO viewing VALUES('CR56','PA14','38131','too small')
+INSERT INTO viewing VALUES('CR76','PG4','38097','too remote')
+INSERT INTO viewing VALUES('CR56','PG4','38133','')
+INSERT INTO viewing VALUES('CR62','PA14','38121','no dining room')
+INSERT INTO viewing VALUES('CR56','PG36','38105','')
