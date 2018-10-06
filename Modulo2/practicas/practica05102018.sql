0a2fa9b1bea6ec207948b42b07f60b54dddd1333
MarcoVH <marcovazquezh@gmail.com>
2018-10-05 20:20:49 -0500

    Add files via upload

--- Modulo2/practicas/practica05102018.sql
+++ Modulo2/practicas/practica05102018.sql
@@ -17,7 +17,7 @@ left outer join pago p on p.id_cliente=c.id_cliente;
 
 #3 
 #GRANT CREATE VIEW TO udiplo;
-create or replace view vista1
+create or replace view vw_ingresos
 	as  
 	select c.id_cliente, c.razon_social_cliente, c.id_division, c.dias_credito_cliente,
 	d.descripcion_division,  f.total_factura fac, 0 pago
@@ -35,6 +35,39 @@ WITH read only;
 #4 
 select razon_social_cliente, sum(fac) as facturado, sum(pago) as pagado,
 sum(fac)-sum(pago) as saldo
-from vista1
+from vw_ingresos
 group by razon_social_cliente
 order by 1;
+
+#5
+select t.id_equipo, t.id_lugar, p.id_pais, desc_pais,
+from torneo t
+left outer join pais p
+on t.id_pais=p.id_pais
+
+#6
+select e.desc_equipo, A.finales as finales, nvl(B.primeros,0) as primeros,
+ nvl(C.segundos,0) as segundos from 
+(select id_equipo, count(id_equipo) as finales 
+from torneo
+group by id_equipo) A
+left outer join 
+(select id_equipo, count(id_equipo) as primeros
+from torneo
+where id_lugar = 1
+group by id_equipo) B
+on A.id_equipo=B.id_equipo
+left outer join 
+(select id_equipo, count(id_equipo) as segundos
+from torneo
+where id_lugar = 2
+group by id_equipo) C
+on A.id_equipo=C.id_equipo
+left outer join 
+equipo e
+on A.id_equipo=e.id_equipo
+order by 3 desc, 2 desc;
+
+
+
+
